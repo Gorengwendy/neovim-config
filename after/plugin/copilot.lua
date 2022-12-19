31daddbea0c4aa9de7ed72318d62b510db97faca
Denzel Wendy <denzel.wendy@binus.ac.id>
2022-12-19 15:30:41 +0700

    Added go to copilot ignore

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,3 +1,3 @@
 -- vim.g.copilot_filetypes = {["*"] = false, ["lua"] = true,["rust"] = true,["php"] = true, ["python"] = true}
-vim.g.copilot_filetypes = {["markdown"] = false}
+vim.g.copilot_filetypes = {["markdown"] = false, ["go"] = false}
 
